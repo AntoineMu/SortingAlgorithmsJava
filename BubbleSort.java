6669d98d12ec7f170bc8405580a13ffb4385ffdc
Anton <anton.lettow@stud.h-da.de>
2022-10-14 04:05:36 +0200

    started working on visualizing the algorithm using lwjgl

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,16 +1,28 @@
-import java.time.Duration;
-import java.time.Instant;
+import SortingAlgorithmsJava.Display.Display;
 import java.util.Random;
+
+import static java.lang.Math.abs;
+import static org.lwjgl.opengl.GL11.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.Callbacks.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.*;
+
 
 public class BubbleSort{
 
     public static void main(String [] args){
 
         Random rd = new Random(); // creating Random object
-        int[] arr = new int[5000];
+        int[] arr = new int[1000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rd.nextInt(); // storing random integers in an array
         }
+
+        Display display = new Display();
+        display.init();
+        loop(display, arr);
 
         long startTime = System.nanoTime();
         bubbleSort(arr);
@@ -37,5 +49,70 @@ public class BubbleSort{
             }
         }
         return arr;
+    }
+
+    private static void loop(Display display, int[] arr) {
+        long window = display.getWindowId();
+
+        GL.createCapabilities();
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        GL11.glMatrixMode(GL11.GL_PROJECTION_MATRIX);
+        glDisable(GL_DEPTH_TEST);
+
+        float barWidth = (float) 1/arr.length;
+
+        int barHeight = -1000000000;
+        for(int i : arr){
+            if(Math.abs(i) > barHeight) barHeight = Math.abs(i);
+        }
+
+
+        while ( !glfwWindowShouldClose(window) ) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+
+
+            for(int j=0; j<arr.length; ++j) {
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                float currBarWidth = 0.f;
+                for (int i = 0; i < arr.length - 1 - j; ++i) {
+                    //TODO: Update display
+                    //draw(currBarWidth, arr[i], barWidth);       //passing the current x and y coordinates and the total width
+
+                    glBegin(GL_QUADS);
+                    glColor3f(currBarWidth, 1-currBarWidth, 1.0f);
+                    glVertex2f(currBarWidth, 0);
+                    glVertex2f(currBarWidth, (float)arr[i]/(float) barHeight);
+                    glVertex2f(currBarWidth+barWidth, (float)arr[i]/(float)barHeight);
+                    glVertex2f(currBarWidth+barWidth, 0);
+                    glEnd();
+                    currBarWidth += barWidth;
+                    if (arr[i] > arr[i + 1]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                    }
+                }
+
+                glfwSwapBuffers(window); // swap the color buffers
+                glfwPollEvents();
+            }
+            //TODO: Update display
+
+            glfwSwapBuffers(window); // swap the color buffers
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
+        }
+    }
+
+    public static void draw(double x, int y, double width){
+        glBegin(GL_QUADS);
+        glVertex2f((float)x, (float)0);
+        glVertex2f((float)(x), y);
+        glVertex2f((float)(x+width), y);
+        glVertex2f((float)(x+width), 0);
+        glEnd();
     }
 }
---
+++ Display/Display.java
@@ -0,0 +1,83 @@
+package SortingAlgorithmsJava.Display;
+
+import org.lwjgl.glfw.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Display {
+    public long getWindowId() {
+        return windowId;
+    }
+
+    private long windowId;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    private int width = 640;
+    private int height = 480;
+
+
+    public void init() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if ( !glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+
+        // Create the window
+        windowId = glfwCreateWindow(width, height, "Visualizer", NULL, NULL);
+        if ( windowId == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(windowId, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+        });
+
+        // Get the thread stack and push a new frame
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(windowId, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    windowId,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(windowId);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(windowId);
+    }
+}
